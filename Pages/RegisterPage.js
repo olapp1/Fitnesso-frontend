720b270178bc538f39feb122017dce82695d0902
Aleksandra Poneta <82601730+olapp1@users.noreply.github.com>
2024-04-07 19:40:47 +0200

    improve register page

--- Pages/RegisterPage.js
+++ Pages/RegisterPage.js
@@ -10,13 +10,41 @@ const RegisterPage = ({ navigation }) => {
   const [password, setPassword] = useState('');
   const [phone, setPhone] = useState('');
 
+  const validateEmail = (email) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  };
+
+  const validatePhone = (phone) => {
+    const re = /^\d{9}$/;
+    return re.test(phone);
+  };
+
   const handleRegister = () => {
+    if (!firstName || !lastName || !email || !login || !password || !phone) {
+      Alert.alert("Błąd", "Wszystkie pola muszą być wypełnione.");
+      return;
+    }
+    if (!validateEmail(email)) {
+      Alert.alert("Błąd", "Niepoprawny format adresu email.");
+      return;
+    }
+    if (password.length < 6) {
+      Alert.alert("Błąd", "Hasło musi zawierać co najmniej 6 znaków.");
+      return;
+    }
+    if (!validatePhone(phone)) {
+      Alert.alert("Błąd", "Numer telefonu musi składać się z 9 cyfr.");
+      return;
+    }
+
     PostRequests.registerUser(firstName, lastName, login, email, password, phone)
       .then(response => {
         Alert.alert("Sukces", "Rejestracja przebiegła pomyślnie.");
-        navigation.navigate('Login');
+        navigation.navigate('Login'); // Upewnij się, że 'Login' odpowiada nazwie ekranu logowania w Twoim Stack Navigatorze.
       })
       .catch(error => {
+        // W praktyce tutaj powinieneś wyświetlić bardziej szczegółowy komunikat błędu zwrócony przez serwer, jeśli to możliwe
         Alert.alert("Błąd rejestracji", "Wystąpił problem podczas rejestracji. Spróbuj ponownie.");
         console.error(error);
       });
@@ -25,48 +53,29 @@ const RegisterPage = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Rejestracja</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Imię"
-        value={firstName}
-        onChangeText={setFirstName}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Nazwisko"
-        value={lastName}
-        onChangeText={setLastName}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        keyboardType="email-address"
-        value={email}
-        onChangeText={setEmail}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Nazwa użytkownika"
-        value={login}
-        onChangeText={setLogin}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Hasło"
-        secureTextEntry={true}
-        value={password}
-        onChangeText={setPassword}
-      />
+      <TextInput style={styles.input} placeholder="Imię" value={firstName} onChangeText={setFirstName} />
+      <TextInput style={styles.input} placeholder="Nazwisko" value={lastName} onChangeText={setLastName} />
+      <TextInput style={styles.input} placeholder="Email" keyboardType="email-address" value={email} onChangeText={setEmail} />
+      <TextInput style={styles.input} placeholder="Nazwa użytkownika" value={login} onChangeText={setLogin} />
+      <TextInput style={styles.input} placeholder="Hasło" secureTextEntry={true} value={password} onChangeText={setPassword} />
       <TextInput
         style={styles.input}
         placeholder="Numer telefonu"
+        keyboardType="numeric"  
         value={phone}
         onChangeText={setPhone}
+        maxLength={9}  
       />
-     
       <TouchableOpacity style={styles.registerButton} onPress={handleRegister}>
         <Text style={styles.buttonText}>Zarejestruj się</Text>
       </TouchableOpacity>
+
+      <View style={styles.loginPrompt}>
+        <Text style={styles.loginPromptText}>Masz konto? </Text>
+        <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+          <Text style={styles.loginPromptLink}>Zaloguj się!</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -92,11 +101,24 @@ const styles = StyleSheet.create({
   },
   registerButton: {
     width: '100%',
-    backgroundColor: '#007BFF', 
+    backgroundColor: '#007BFF',
     padding: 10,
     alignItems: 'center',
     borderRadius: 5,
     marginBottom: 10,
+  },
+  loginPrompt: {
+    flexDirection: 'row', 
+    marginTop: 20, 
+  },
+  loginPromptText: {
+    fontSize: 16, 
+    color: 'gray', 
+  },
+  loginPromptLink: {
+    fontSize: 16, 
+    color: '#007BFF', 
+    fontWeight: 'bold', 
   },
   buttonText: {
     color: 'white',
